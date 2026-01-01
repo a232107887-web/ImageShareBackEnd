3e19e0d8f415dfc3b39340aca452044776b5a6a5
a232107887-web <a232107887@gmail.com>
2026-01-01 16:35:15 +0800

    MongoDB兼容问题

--- src/main/java/com/imageshare/model/Image.java
+++ src/main/java/com/imageshare/model/Image.java
@@ -1,11 +1,8 @@
 package com.imageshare.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
 
 @Data
 @Document(collection = "images")
@@ -24,12 +21,7 @@ public class Image {
     private String contentType;
     
     private Long fileSize;
-    
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime uploadDate;
-    
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime updatedAt;
+
 }
 
 
